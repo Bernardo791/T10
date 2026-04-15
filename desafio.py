8b774e67c7ca697a78c9a64f862a84662a0a210f
Bernardo791 <bernardomachadodospassos@gmail.com>
2026-04-15 17:05:12 -0300

    Add files via upload

---
+++ desafio.py
@@ -0,0 +1,16 @@
+# Criando uma lista de alunos
+animais = ["Cachorro", "Gato", "Peixe"]
+
+# Exibindo a lista completa
+print(f"Lista de animais: {animais}")
+
+# Acessando o primeiro animal (indice 0)
+print(f"Primeiro animal: {animais[0]}")
+
+# Acessando ultimo animal (indice -1)
+print(f"Ultimo animal: {animais[-1]}")
+
+# Adicionar: insere um novo animal no final da lista
+animais.append("Hamster")
+print(f"Lista atualizada: {animais}")
+
