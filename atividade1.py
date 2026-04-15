f35cb3baddde5700a5a36b30739e66fed6839c53
Bernardo791 <bernardomachadodospassos@gmail.com>
2026-04-15 16:44:05 -0300

    Add files via upload

---
+++ atividade1.py
@@ -0,0 +1,9 @@
+# Exercicio 1: Lista de compras
+compras = ["Pao", "Leite", "Cafe"]
+print(f"Lista Completa: {compras}")
+
+# Acessando o primeiro item (indice 0)
+print(f"Primeiro item: {compras[0]}")
+
+# Acessando o último item (indice -1)
+print(f"Último item: {compras[-1]}")
---
+++ atividade3.py
@@ -0,0 +1,9 @@
+# Criando um dicionario com meus dados
+pessoa = {
+    "nome": "Bernardo",
+    "idade": 16,
+    "cidade": "Juiz de Fora"
+}
+
+print(pessoa)
+print(f"Tipo: {type(pessoa)}")
